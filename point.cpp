648d6e939b9de0dd1d44480342a7dc3c594956a4
Ahmed Gamal <155440976+JEMYJYMAT@users.noreply.github.com>
2024-01-30 13:07:41 +0200

    Create point.cpp

---
+++ point.cpp
@@ -0,0 +1,20 @@
+/*
+ * point.cpp
+ *
+ *  Created on: Jan 29, 2024
+ *      Author: ahmed gamal
+ */
+
+#include "point.h"
+
+point::point()
+{
+	x = 0;
+	y = 0;
+}
+
+point::point(float x_cor, float y_cor)
+{
+	x = x_cor;
+	y = y_cor;
+}
